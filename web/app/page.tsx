0b670d44b09efb243350ee694d18d2fd4f4d8087
jankoukl0-svg <jankoukl0@gmail.com>
2026-09-16 21:59:38 +0200

    feat: mount missed-task resolver

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,6 @@
 import { DayframeApp } from "./dayframe-app";
+import { MissedTaskActions } from "./missed-task-actions";
 
 export default function Home() {
-  return <DayframeApp />;
+  return <><DayframeApp /><MissedTaskActions /></>;
 }
