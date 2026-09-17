97e51d1bc26c6511462fcac22765ed2fc48bb644
jankoukl0-svg <jankoukl0@gmail.com>
2026-09-17 14:37:23 +0200

    fix: clean capture confirmation title

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,10 @@
 import { DayframeApp } from "./dayframe-app";
 import { CaptureStartControl } from "./capture-start-control";
+import { CaptureResultCleaner } from "./capture-result-cleaner";
 import { MissedTaskActions } from "./missed-task-actions";
 import { WeekCalendar } from "./week-calendar";
 import "./week-calendar.css";
 
 export default function Home() {
-  return <><DayframeApp /><CaptureStartControl /><MissedTaskActions /><WeekCalendar /></>;
+  return <><DayframeApp /><CaptureStartControl /><CaptureResultCleaner /><MissedTaskActions /><WeekCalendar /></>;
 }
