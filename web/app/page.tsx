ba59ef5cb13e69540df2ed75b97a42326889bec9
jankoukl0-svg <jankoukl0@gmail.com>
2026-09-17 13:46:19 +0200

    feat: mount weekly calendar

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,9 @@
 import { DayframeApp } from "./dayframe-app";
 import { CaptureStartControl } from "./capture-start-control";
 import { MissedTaskActions } from "./missed-task-actions";
+import { WeekCalendar } from "./week-calendar";
+import "./week-calendar.css";
 
 export default function Home() {
-  return <><DayframeApp /><CaptureStartControl /><MissedTaskActions /></>;
+  return <><DayframeApp /><CaptureStartControl /><MissedTaskActions /><WeekCalendar /></>;
 }
