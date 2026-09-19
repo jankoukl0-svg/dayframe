3e7837415de38f1e18565ece6e85ea14edcdf6b4
jankoukl0-svg <jankoukl0@gmail.com>
2026-09-20 00:29:11 +0200

    Align milestone countdown columns
    * Align milestone countdown columns
    
    * Load milestone alignment styles

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,6 +2,7 @@ import { DayframeV2 } from "./dayframe-v2";
 import "./dayframe-v2.css";
 import "./dayframe-countdowns.css";
 import "./week-calendar-polish.css";
+import "./milestone-alignment.css";
 
 export default function Home() {
   return <DayframeV2 />;
